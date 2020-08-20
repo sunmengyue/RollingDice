b4fefaf448834763bd9c746a5400c30538c7e1d9
Simon <bardrabbit@gmail.com>
2020-08-20 06:27:50 -0400

    add rolling animation effect

--- src/RollDice.js
+++ src/RollDice.js
@@ -9,7 +9,7 @@ class RollDice extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { dice1: "one", dice2: "one" };
+    this.state = { dice1: "one", dice2: "one", isRolling: false };
     this.roll = this.roll.bind(this);
   }
 
@@ -23,17 +23,24 @@ class RollDice extends Component {
     this.setState({
       dice1: newDice1,
       dice2: newDice2,
+      isRolling: true,
     });
+
+    setTimeout(() => {
+      this.setState({ isRolling: false });
+    }, 1000);
   }
 
   render() {
     return (
       <div className="roll-dice">
         <div className="dices">
-          <Die face={this.state.dice1} />
-          <Die face={this.state.dice2} />
+          <Die face={this.state.dice1} rolling={this.state.isRolling} />
+          <Die face={this.state.dice2} rolling={this.state.isRolling} />
         </div>
-        <button onClick={this.roll}>Rolling</button>
+        <button onClick={this.roll} disabled={this.state.isRolling}>
+          {this.state.isRolling ? `Rolling...` : `Roll Dice!`}
+        </button>
       </div>
     );
   }
