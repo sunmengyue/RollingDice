4bd6c49aaa0f42a2da8c374e2b2cf2b219f52f49
Simon <bardrabbit@gmail.com>
2020-08-20 01:18:00 -0400

    create roll function and styling the roll button

--- src/RollDice.js
+++ src/RollDice.js
@@ -3,17 +3,37 @@ import Die from "./Die";
 import "./RollDice.css";
 
 class RollDice extends Component {
+  static defaultProps = {
+    faces: ["one", "two", "three", "four", "five", "six"],
+  };
+
   constructor(props) {
     super(props);
+    this.state = { dice1: "one", dice2: "one" };
+    this.roll = this.roll.bind(this);
   }
+
+  roll() {
+    const newDice1 = this.props.faces[
+      Math.floor(Math.random() * this.props.faces.length)
+    ];
+    const newDice2 = this.props.faces[
+      Math.floor(Math.random() * this.props.faces.length)
+    ];
+    this.setState({
+      dice1: newDice1,
+      dice2: newDice2,
+    });
+  }
+
   render() {
     return (
       <div className="roll-dice">
         <div className="dices">
-          <Die face="five" />
-          <Die face="two" />
+          <Die face={this.state.dice1} />
+          <Die face={this.state.dice2} />
         </div>
-        <button>Rolling</button>
+        <button onClick={this.roll}>Rolling</button>
       </div>
     );
   }
